e13fa40c6acce58b97cd06282944b771513d275f
Jamie Blair <info@orangemug.co.uk>
2025-01-28 07:08:15 +0000

    Fixed version in CLI

--- bin/ooxml-file.ts
+++ bin/ooxml-file.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env ./node_modules/.bin/tsx
 import yargs from "yargs/yargs";
 import * as commands from "../src/commands";
+import { version } from "../package.json";
 
 const scriptName = "ooxml-file";
 
@@ -58,5 +59,6 @@ yargs(process.argv.slice(2))
     commands.write.builder,
     commands.write.handler,
   )
+  .version(version)
   .demand(1)
   .help().argv;
