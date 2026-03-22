2dd3ebc14605a7a0a307fa01b455bbf0946aca63
Jamie Blair <info@orangemug.co.uk>
2026-03-22 15:31:20 +0000

    fix: param names in cli

--- src/commands/read.ts
+++ src/commands/read.ts
@@ -3,32 +3,32 @@ import { formatFromFilename, open } from "src";
 import { ArgumentsCamelCase, Argv } from "yargs";
 import { openAsArrayBuffer } from "./helper";
 
-export const cmd = "read <docxpath> <ooxmlpath>";
+export const cmd = "read <ooxmlpath> <filepath>";
 export const desc = "read file inside docx to sdtout";
 export const builder = (yargs: Argv) => {
   yargs
-    .positional("docxpath", {
+    .positional("ooxmlpath", {
       type: "string",
       describe: "",
     })
-    .positional("ooxmlpath", {
+    .positional("filepath", {
       type: "string",
       describe: "",
     });
 };
 export async function handler({
-  docxpath,
   ooxmlpath,
-}: ArgumentsCamelCase<{ docxpath: string; ooxmlpath: string }>) {
+  filepath,
+}: ArgumentsCamelCase<{ ooxmlpath: string; filepath: string }>) {
   const zip = new JSZip();
-  await zip.loadAsync(openAsArrayBuffer(docxpath));
-  const doc = open(formatFromFilename(docxpath), zip);
+  await zip.loadAsync(openAsArrayBuffer(ooxmlpath));
+  const doc = open(formatFromFilename(ooxmlpath), zip);
 
-  if (!doc.list().includes(ooxmlpath)) {
-    console.error(`Missing file: ${ooxmlpath}`);
+  if (!doc.list().includes(filepath)) {
+    console.error(`Missing file: ${filepath}`);
     process.exit(1);
   } else {
-    const content = await doc.readFile(ooxmlpath, "uint8array");
+    const content = await doc.readFile(filepath, "uint8array");
     const stringContent = new TextDecoder().decode(content);
     console.log(stringContent);
     process.exit(0);
