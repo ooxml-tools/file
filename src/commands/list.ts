8feacec3c3d554c482c874ea6bf0ba174497e32a
Jamie Blair <info@orangemug.co.uk>
2026-03-22 08:16:00 +0000

    fix: update usage away from docx only

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -3,19 +3,19 @@ import { formatFromFilename, open } from "../";
 import { ArgumentsCamelCase, Argv } from "yargs";
 import { openAsArrayBuffer } from "./helper";
 
-export const cmd = "list <docxpath>";
+export const cmd = "list <filepath>";
 export const desc = "list files in docx";
 export const builder = (yargs: Argv) => {
-  yargs.positional("docxpath", {
+  yargs.positional("ooxmlpath", {
     type: "string",
     describe: "",
   });
 };
 export async function handler({
-  docxpath,
-}: ArgumentsCamelCase<{ docxpath: string }>) {
+  ooxmlpath,
+}: ArgumentsCamelCase<{ ooxmlpath: string }>) {
   const zip = new JSZip();
-  await zip.loadAsync(await openAsArrayBuffer(docxpath));
-  const doc = open(formatFromFilename(docxpath), zip);
+  await zip.loadAsync(await openAsArrayBuffer(ooxmlpath));
+  const doc = open(formatFromFilename(ooxmlpath), zip);
   console.log(doc.list().join("\n"));
 }
--- src/commands/write.ts
+++ src/commands/write.ts
@@ -4,11 +4,11 @@ import { formatFromFilename, open } from "../";
 import { ArgumentsCamelCase, Argv } from "yargs";
 import { openAsArrayBuffer } from "./helper";
 
-export const cmd = "write <docxpath> <filepath>";
+export const cmd = "write <ooxmlpath> <filepath>";
 export const desc = "create/override file in docx";
 export const builder = (yargs: Argv) => {
   yargs
-    .positional("docxpath", {
+    .positional("ooxmlpath", {
       type: "string",
       describe: "",
     })
@@ -18,9 +18,9 @@ export const builder = (yargs: Argv) => {
     });
 };
 export async function handler({
-  docxpath,
+  ooxmlpath,
   filepath,
-}: ArgumentsCamelCase<{ docxpath: string; filepath: string }>) {
+}: ArgumentsCamelCase<{ ooxmlpath: string; filepath: string }>) {
   const buffers = [];
   for await (const data of process.stdin) {
     buffers.push(data);
@@ -28,11 +28,11 @@ export async function handler({
   const inputData = new Blob([Buffer.concat(buffers)]);
 
   const zip = new JSZip();
-  await zip.loadAsync(openAsArrayBuffer(docxpath));
-  const doc = open(formatFromFilename(docxpath), zip);
+  await zip.loadAsync(openAsArrayBuffer(ooxmlpath));
+  const doc = open(formatFromFilename(ooxmlpath), zip);
 
   doc.writeFile(filepath, await inputData.arrayBuffer());
 
   const buffer = await doc.pack("uint8array");
-  await writeFile(docxpath, buffer);
+  await writeFile(ooxmlpath, buffer);
 }
